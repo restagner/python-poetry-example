83b41427a7efdc2cc2849f17c1bc1cb4ef66a40d
Robert Stagner <robert.stagner@disney.com>
2022-09-05 18:01:26 -0700

    added tests and updated main script

---
+++ src/tests/test_console.py
@@ -0,0 +1,13 @@
+from python_poetry_example import __version__
+from python_poetry_example import console
+import click.testing
+
+
+def test_version():
+    assert __version__ == "0.1.0"
+
+
+def test_main_succeeds():
+    runner = click.testing.CliRunner()
+    result = runner.invoke(console.hello)
+    assert result.exit_code == 0
